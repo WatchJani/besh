6eea59acfc2149bd7e2f48daebb866de64efd3b9
Janko <45310863+WatchJani@users.noreply.github.com>
2022-11-07 20:43:14 +0100

    Add files via upload

--- domaca_naloga_praktikum.sh
+++ domaca_naloga_praktikum.sh
@@ -3,6 +3,22 @@
 
 read -p 'unesite dimenzije piramide: ' size
 
+# for ((i=1;i<size+1;i++))
+# do 
+#     for ((j=1; j < size+2; j++))
+#     do
+#         if [ $j -le $(($size+1-$i)) ]
+#         then
+#              echo -n ' '
+#             else
+#             echo -n " $i"
+#         fi
+#     done
+#     echo
+# done
+
+# =============================================================================
+
 for ((i=1;i<size+1;i++))
 do 
     for ((j=1; j < size+2; j++))
@@ -11,7 +27,7 @@ do
         then
              echo -n ' '
             else
-            echo -n " $i"
+            echo -n ' *'
         fi
     done
     echo
