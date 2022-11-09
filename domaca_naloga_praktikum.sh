8e8c297d742552e301b8215b4a64363b90b606bc
Janko <45310863+WatchJani@users.noreply.github.com>
2022-11-09 12:36:04 +0100

    Add files via upload

--- domaca_naloga_praktikum.sh
+++ domaca_naloga_praktikum.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 
+#boljsa sintaksa
+
+# if(($size <= 5 && $size*2-4 == 4))
+# then
+#     echo "manjše je "
+# fi
+
 read -p 'unesite dimenzije piramide: ' size
 
 # for ((i=1;i<size+1;i++))
@@ -19,16 +26,39 @@ read -p 'unesite dimenzije piramide: ' size
 
 # =============================================================================
 
-for ((i=1;i<size+1;i++))
-do 
-    for ((j=1; j < size+2; j++))
-    do
-        if [ $j -le $(($size+1-$i)) ]
-        then
-             echo -n ' '
-            else
-            echo -n ' *'
-        fi
-    done
-    echo
-done
+# for ((i=1;i<size+1;i++))
+# do 
+#     for ((j=1; j < size+2; j++))
+#     do
+#         if [ $j -le $(($size+1-$i)) ]
+#         then
+#              echo -n ' '
+#             else
+#             echo -n ' *'
+#         fi
+#     done
+#     echo
+# done
+
+# =============================================================================
+
+
+# size=$((size-1))
+
+# for ((i=0;i<(size+1)*2;i++))
+# do
+#     for ((j=0; j < size+1; j++))
+#     do
+#         if [ $j -le $i ] && [ $i -le $(($size+1)) ] 
+#         then
+#             echo -n ' *'
+#         elif [ $i -ge $size ] && [ $j -le $((size*2-i+1)) ] //+1 ker ne vem kako napisati <= :D
+#         then
+#             echo -n ' *'
+#         fi
+#     done
+#     echo
+# done
+
+# =============================================================================
+
